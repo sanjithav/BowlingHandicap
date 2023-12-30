29ded2dd1d2a8c64fec162fc59077e17a9dea601
sanjithav <145514744+sanjithav@users.noreply.github.com>
2023-12-29 19:21:15 -0600

    Add files via upload

---
+++ Handicap.py
@@ -0,0 +1,29 @@
+# File: Handicap.py
+# Student: Sanjitha Venkata
+# UT EID: sv28325
+# Course Name: CS303E
+# 
+# Date: 9/2/2023
+# Description of Program: Takes user inputs of name and 3 game scores, calculates average and handicap, outputs handicap report for name given
+
+from math import floor #imports function needed to round numbers
+
+#takes user input
+name=input("Enter bowler's name: ")
+game1=int(input("Enter Game 1: "))
+game2=int(input("Enter Game 2: "))
+game3=int(input("Enter Game 3: "))
+
+#calculates avg and handicap
+average=floor((game1+game2+game3)/3)
+handicap=floor((200-average)*0.8)
+handicap=max(0,handicap)
+
+#prints output
+print()
+print("Handicap report for ",name,":",sep="")
+print("  ",name,"'s average is: ",average,sep="")
+print("  ",name,"'s handicap is: ",handicap,sep="")
+print()
+print("It's time to Bowl!")
+print()
